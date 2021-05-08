c90e6be1692a60a724569e9a990b8b218ad2f7ef
Robson Santos <robson.eng@hotmail.com>
2021-05-07 22:10:45 -0300

    Created empresas app

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .keys import SECRET_KEY as SK
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1a7qr+c^_=8^xp=@+go8*j#ps(nxy#l6p9t2m5z6=)jn=!x%!u'
+SECRET_KEY = SK
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'empresas'
 ]
 
 MIDDLEWARE = [
